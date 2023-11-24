2f081b130ab242d8dd650bf8ee73b94ed951157a
Benjent <Benjent@hotmail.fr>
2023-11-24 19:16:08 +0100

    chore: run eslint --fix on repo

--- src/stores/employee.js
+++ src/stores/employee.js
@@ -1,13 +1,13 @@
-import { reactive, ref } from 'vue'
-import { defineStore } from 'pinia'
+import { ref } from "vue"
+import { defineStore } from "pinia"
 
-export const useEmployeeStore = defineStore('employee', () => {
-  const employeeFullName = ref("MORVAN Benjamin Jean")
-  const employeeId = ref("011")
-  const companyName = ref("SCOP Les Octets Libres")
-  const companyId = ref("06753")
-  const workingDays = ref([1, 2, 3, 4, 5])
-  const workingHourCount = ref(7)
+export const useEmployeeStore = defineStore("employee", () => {
+    const employeeFullName = ref("MORVAN Benjamin Jean")
+    const employeeId = ref("011")
+    const companyName = ref("SCOP Les Octets Libres")
+    const companyId = ref("06753")
+    const workingDays = ref([1, 2, 3, 4, 5])
+    const workingHourCount = ref(7)
 
-  return { companyId, companyName, employeeFullName, employeeId, workingDays, workingHourCount }
+    return { companyId, companyName, employeeFullName, employeeId, workingDays, workingHourCount }
 })
--- src/stores/period.js
+++ src/stores/period.js
@@ -1,11 +1,11 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue"
+import { defineStore } from "pinia"
 import { format } from "date-fns"
 
-export const usePeriodStore = defineStore('period', () => {
+export const usePeriodStore = defineStore("period", () => {
     const periodCode = ref(format(new Date(), "yy MM"))
-    const periodYear = computed(() => periodCode.split(" ")?.[0])
-    const periodMonth = computed(() => periodCode.split(" ")?.[1])
+    const periodYear = computed(() => periodCode.value.split(" ")?.[0])
+    const periodMonth = computed(() => periodCode.value.split(" ")?.[1])
 
-  return { periodCode, periodMonth, periodYear }
+    return { periodCode, periodMonth, periodYear }
 })
