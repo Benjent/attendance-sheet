3b940e42435baf091c663951c11e555172c758da
Benjent <Benjent@hotmail.fr>
2023-11-24 23:37:45 +0100

    fix(stores/employee): put back reactive instead of ref for workingDays array

--- src/stores/employee.js
+++ src/stores/employee.js
@@ -1,4 +1,4 @@
-import { ref } from "vue"
+import { reactive, ref } from "vue"
 import { defineStore } from "pinia"
 
 export const useEmployeeStore = defineStore("employee", () => {
@@ -6,7 +6,7 @@ export const useEmployeeStore = defineStore("employee", () => {
     const employeeId = ref("011")
     const companyName = ref("SCOP Les Octets Libres")
     const companyId = ref("06753")
-    const workingDays = ref([1, 2, 3, 4, 5])
+    const workingDays = reactive([1, 2, 3, 4, 5])
     const workingHourCount = ref(7)
 
     return { companyId, companyName, employeeFullName, employeeId, workingDays, workingHourCount }
