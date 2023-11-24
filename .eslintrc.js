4f6b4c78ece04f762b90de4764f5d45577ade7db
Benjent <Benjent@hotmail.fr>
2023-11-24 19:48:08 +0100

    chore(lint): add and run eslint with dangling coma always-multiline rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,36 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true
+        es2021: true,
     },
     extends: [
         "standard",
-        "plugin:vue/vue3-essential"
+        "plugin:vue/vue3-essential",
     ],
     overrides: [
         {
             env: {
-                node: true
+                node: true,
             },
             files: [
-                ".eslintrc.{js,cjs}"
+                ".eslintrc.{js,cjs}",
             ],
             parserOptions: {
-                sourceType: "script"
-            }
-        }
+                sourceType: "script",
+            },
+        },
     ],
     parserOptions: {
         ecmaVersion: "latest",
-        sourceType: "module"
+        sourceType: "module",
     },
     plugins: [
-        "vue"
+        "vue",
     ],
     rules: {
+        "comma-dangle": ["error", "always-multiline"],
         indent: ["error", 4],
         quotes: ["error", "double"],
-        semi: ["error", "never"]
-    }
+        semi: ["error", "never"],
+    },
 }
