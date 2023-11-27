e91983dec2d7f840c7112b2672b3ff38d106eb2a
Benjent <Benjent@hotmail.fr>
2023-11-27 21:39:26 +0100

    feat(Form.vue): use vertical UI to be more responsive for small widths screen

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -37,92 +37,90 @@ function printPreview () {
 </script>
 
 <template>
-    <form class="print:hidden w-auto flex flex-col flex-1 p-10 gap-10 justify-center items-center bg-slate-900 rounded-2xl shadow-2xl text-slate-100 italic overflow-y-auto">
+    <form class="print:hidden h-screen w-auto flex flex-1 p-10 gap-10 justify-center items-center bg-slate-900 rounded-2xl shadow-2xl text-slate-100 italic overflow-y-auto">
         <fieldset class="flex flex-col gap-5 items-center">
-            <div class="flex flex-col lg:flex-row gap-5">
-                <div class="flex flex-col gap-5">
-                    <label class="flex flex-col text-rose-300">
-                        Nom de l'entreprise
-                        <input v-model="companyName" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+            <div class="flex flex-col gap-5">
+                <label class="flex flex-col text-rose-300">
+                    Nom de l'entreprise
+                    <input v-model="companyName" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                </label>
+                <label class="flex flex-col text-rose-300">
+                    ID de l'entreprise
+                    <input v-model="companyId" :maxlength="COMPANY_ID_LENGTH" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                </label>
+                <label class="flex flex-col text-rose-300">
+                    Nom complet de l'employé
+                    <input v-model="employeeFullName" placeholder="NOM DE FAMILLE Prénoms" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                </label>
+                <label class="flex flex-col text-rose-300">
+                    ID de l'employé
+                    <input v-model="employeeId" :maxlength="EMPLOYEE_ID_LENGTH" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                </label>
+                <label class="flex flex-col text-rose-300">
+                    Signature de l'employé
+                    <p class="text-white">
+                        Penser à ajouter
+                        <br />
+                        <span class="font-mono text-sm text-cyan-400">
+                            class="<span class="text-amber-400">h-full w-full</span>"
+                        </span>
+                        <br />
+                        au tag svg
+                    </p>
+                    <textarea v-model="employeeSignature" placeholder="<svg class='h-full w-full'>...</svg>" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                </label>
+                <label class="flex flex-col text-rose-300">
+                    Heures travaillées par défaut
+                    <input v-model.number="workingHourCount" type="number" class="w-20 mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                </label>
+                <fieldset class="flex flex-col text-rose-300">
+                    <legend>Jours travaillés par défaut</legend>
+                    <label>
+                        <input v-model="workingDays" type="checkbox" :value="1" />
+                        Lundi
                     </label>
-                    <label class="flex flex-col text-rose-300">
-                        ID de l'entreprise
-                        <input v-model="companyId" :maxlength="COMPANY_ID_LENGTH" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                    <label>
+                        <input v-model="workingDays" type="checkbox" :value="2" />
+                        Mardi
                     </label>
-                </div>
-                <div class="flex flex-col gap-5">
-                    <label class="flex flex-col text-rose-300">
-                        Nom complet de l'employé
-                        <input v-model="employeeFullName" placeholder="NOM DE FAMILLE Prénoms" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                    <label>
+                        <input v-model="workingDays" type="checkbox" :value="3" />
+                        Mercredi
                     </label>
-                    <label class="flex flex-col text-rose-300">
-                        ID de l'employé
-                        <input v-model="employeeId" :maxlength="EMPLOYEE_ID_LENGTH" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                    <label>
+                        <input v-model="workingDays" type="checkbox" :value="4" />
+                        Jeudi
                     </label>
-                    <label class="flex flex-col text-rose-300">
-                        Signature de l'employé
-                        <p class="text-white">
-                            Penser à ajouter
-                            <span class="font-mono text-sm text-cyan-400">
-                                class="<span class="text-amber-400">h-full w-full</span>"
-                            </span>
-                            au tag svg
-                        </p>
-                        <textarea v-model="employeeSignature" placeholder="<svg class='h-full w-full'>...</svg>" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                    <label>
+                        <input v-model="workingDays" type="checkbox" :value="5" />
+                        Vendredi
                     </label>
-                </div>
-                <div class="flex flex-col gap-5">
-                    <label class="flex flex-col text-rose-300">
-                        Heures travaillées par défaut
-                        <input v-model.number="workingHourCount" type="number" class="w-20 mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
-                    </label>
-                    <fieldset class="flex flex-col text-rose-300">
-                        <legend>Jours travaillés par défaut</legend>
-                        <label>
-                            <input v-model="workingDays" type="checkbox" :value="1" />
-                            Lundi
-                        </label>
-                        <label>
-                            <input v-model="workingDays" type="checkbox" :value="2" />
-                            Mardi
-                        </label>
-                        <label>
-                            <input v-model="workingDays" type="checkbox" :value="3" />
-                            Mercredi
-                        </label>
-                        <label>
-                            <input v-model="workingDays" type="checkbox" :value="4" />
-                            Jeudi
-                        </label>
-                        <label>
-                            <input v-model="workingDays" type="checkbox" :value="5" />
-                            Vendredi
-                        </label>
-                    </fieldset>
-                </div>
+                </fieldset>
             </div>
-            <button class="py-2 px-4 bg-transparent bg-gray-600 hover:bg-slate-500 font-semibold rounded shadow" @click="saveEmployeeDetails" type="submit">Sauvegarder les informations</button>
+            <button class="py-2 px-4 bg-slate-600 hover:bg-slate-500 font-semibold rounded shadow" @click="saveEmployeeDetails" type="submit">Sauvegarder les informations</button>
         </fieldset>
-        <fieldset class="flex gap-5 items-center">
-            <label class="flex flex-col text-rose-300">
-                Période
-                <input v-model="periodCode" placeholder="AA MM" :maxlength="PERIOD_CODE_LENGTH" class="w-20 mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
-            </label>
-            <label class="flex flex-col text-rose-300">
-                Comptable
-                <input v-model="accountant" placeholder="Prénom" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
-            </label>
-        </fieldset>
-        <footer class="flex flex-col gap-4 items-center">
-            <p>Penser à désactiver les marges dans la fenêtre d'impression.</p>
-            <button class="w-full py-2 px-4 bg-rose-800 hover:bg-rose-700 font-semibold text-white rounded shadow" @click="printPreview" type="submit">Générer pdf</button>
-            <div class="flex flex-col gap-2">
-                <p>Meilleur rendu avec</p>
-                <div class="flex gap-2 justify-center">
-                    <img class="max-h-10" src="@/assets/images/logos/brave.png" alt="Logo du navigateur web Brave" />
-                    <img class="max-h-10" src="@/assets/images/logos/google-chrome.png" alt="Logo du navigateur web Google Chrome" />
+        <div class="flex flex-col gap-5">
+            <fieldset class="flex flex-col gap-5">
+                <label class="flex flex-col text-rose-300">
+                    Période
+                    <input v-model="periodCode" placeholder="AA MM" :maxlength="PERIOD_CODE_LENGTH" class="w-20 mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                </label>
+                <label class="flex flex-col text-rose-300">
+                    Comptable
+                    <input v-model="accountant" placeholder="Prénom" class="mt-2 py-1 px-2 rounded border border-b-2 border-b-rose-300 border-slate-600 bg-slate-700 text-slate-100" />
+                </label>
+            </fieldset>
+            <footer class="flex flex-col gap-1 items-center">
+                <p>Penser à désactiver les marges dans la fenêtre d'impression.</p>
+                <button class="w-full py-2 px-4 bg-rose-800 hover:bg-rose-700 font-semibold text-white rounded shadow" @click="printPreview" type="submit">Générer pdf</button>
+                <div class="flex flex-col gap-2">
+                    <p>Meilleur rendu avec</p>
+                    <div class="flex gap-2 justify-center">
+                        <img class="max-h-10" src="@/assets/images/logos/brave.png" alt="Logo du navigateur web Brave" />
+                        <img class="max-h-10" src="@/assets/images/logos/google-chrome.png" alt="Logo du navigateur web Google Chrome" />
+                    </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </div>
     </form>
 </template>
